fc5cbc32254dde05db7b90f8a531e467ad545ece
ali kamran <ali.kamran.98.98@gmail.com>
2023-06-24 13:43:56 +0330

    Button compose

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -1,24 +1,39 @@
 package com.example.composeapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
@@ -26,150 +41,127 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BoxExample3()
+            Column(
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                ButtonDemo()
+            }
         }
-    }
-}
-@Composable
-fun BoxExample1(){
-    Box(modifier = Modifier
-        .background(color = Color.Green)
-        .size(180.dp, 300.dp)
-    ){
-        Box(modifier = Modifier
-            .background(
-                color = Color.Yellow
-            )
-            .size(125.dp, 100.dp)
-            .align(Alignment.TopEnd)
-        ){
-
-        }
-        Text(text = "HI",
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.background(color = Color.White)
-                .size(90.dp,50.dp)
-                .align(Alignment.BottomCenter)
-        )
     }
 }
 
 @Composable
-fun BoxExample2() {
-    Box(
-        modifier = Modifier
-            .background(color = Color.LightGray)
-            .fillMaxSize()
+fun ButtonDemo() {
+    val context = LocalContext.current
+    Button(onClick = {
+        Toast.makeText(context, "Clicked on Button", Toast.LENGTH_SHORT).show()
+    }) {
+        Text("Add To Cart")
+    }
+
+    Button(
+        onClick = {
+            Toast.makeText(context, "Clicked on Button", Toast.LENGTH_SHORT).show()
+        },
+        enabled = false
     ) {
+        Text("Add To Cart")
+    }
 
-        Text(
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier
-                .background(Color.Yellow)
-                .padding(10.dp)
-                .align(Alignment.TopStart),
-            text = "TopStart"
-        )
-        Text(
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier
-                .background(Color.Yellow)
-                .padding(10.dp)
-                .align(Alignment.TopCenter),
-            text = "TopCenter"
-        )
-        Text(
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier
-                .background(Color.Yellow)
-                .padding(10.dp)
-                .align(Alignment.TopEnd),
-            text = "TopEnd"
-        )
+    TextButton(onClick = {
+        Toast.makeText(context, "Clicked on Text Button", Toast.LENGTH_SHORT).show()
+    }) {
+        Text("Add To Cart")
+    }
 
-        Text(
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier
-                .background(Color.Yellow)
-                .padding(10.dp)
-                .align(Alignment.CenterStart),
-            text = "CenterStart"
-        )
+    OutlinedButton(onClick = {
+        Toast.makeText(context, "Clicked on Out Lined Button", Toast.LENGTH_SHORT).show()
+    }) {
+        Text("Add To Cart")
+    }
 
-        Text(
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier
-                .background(Color.Yellow)
-                .padding(10.dp)
-                .align(Alignment.Center),
-            text = "Center"
-        )
-        Text(
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier
-                .background(Color.Yellow)
-                .padding(10.dp)
-                .align(Alignment.CenterEnd),
-            text = "CenterEnd"
-        )
-
-        Text(
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier
-                .background(Color.Yellow)
-                .padding(10.dp)
-                .align(Alignment.BottomStart),
-            text = "BottomStart"
-        )
-        Text(
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier
-                .background(Color.Yellow)
-                .padding(10.dp)
-                .align(Alignment.BottomCenter),
-            text = "BottomCenter"
-        )
-        Text(
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier
-                .background(Color.Yellow)
-                .padding(10.dp)
-                .align(Alignment.BottomEnd),
-            text = "BottomEnd"
+    IconButton(onClick = {
+        Toast.makeText(
+            context,
+            "Clicked on Icon Button",
+            Toast.LENGTH_SHORT
+        ).show()
+    }) {
+        Icon(
+            Icons.Filled.Refresh,
+            contentDescription = "Refresh Button",
+            tint = Color.DarkGray,
+            modifier = Modifier.size(80.dp)
         )
     }
-}
 
-@Composable
-fun BoxExample3() {
-    Box() {
-        Image(
-            painter = painterResource(id = R.drawable.beach_resort),
-            contentDescription = "beach resort"
+    Button(onClick = {
+        Toast.makeText(context, "Clicked on Button", Toast.LENGTH_SHORT).show()
+    },
+        contentPadding = PaddingValues(16.dp),
+        border = BorderStroke(1.dp, Color.Black),
+        colors = ButtonDefaults.textButtonColors(
+            colorResource(id = R.color.purple_200),
+            contentColor = Color.White
         )
-        Text(
-            text = "Beach Resort",
+    ) {
+        Text("Add To Cart",
             style = MaterialTheme.typography.bodyMedium,
-            color = Color.Black,
-            modifier = Modifier
-                .align(Alignment.BottomStart)
+            modifier = Modifier.padding(5.dp)
         )
-        Button(
-            onClick = {},
-            colors = ButtonDefaults.buttonColors(
-                contentColor = Color.DarkGray
-            ),
-            modifier = Modifier
-                .align(Alignment.TopEnd)
-                .padding(10.dp)
-                .border(5.dp, Color.DarkGray, RectangleShape)
-
-        ) {
-            Text("Add To Cart")
-        }
-
     }
+
+    Button(onClick = {
+        Toast.makeText(context, "Clicked on Button", Toast.LENGTH_SHORT).show()
+    },
+        shape = CutCornerShape(10.dp),
+        contentPadding = PaddingValues(16.dp),
+        border = BorderStroke(10.dp, Color.Black),
+        colors = ButtonDefaults.textButtonColors(
+            colorResource(id = R.color.purple_200),
+            contentColor = Color.White
+        )
+    ) {
+        Text("Add To Cart",
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(5.dp)
+        )
+    }
+
+    Button(onClick = {
+        Toast.makeText(context, "Clicked on Button", Toast.LENGTH_SHORT).show()
+    },
+        shape = CircleShape,
+        contentPadding = PaddingValues(16.dp),
+        border = BorderStroke(10.dp, Color.Black),
+        colors = ButtonDefaults.textButtonColors(
+            colorResource(id = R.color.purple_200),
+            contentColor = Color.White
+        )
+    ) {
+        Text("Add To Cart",
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(5.dp)
+        )
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
