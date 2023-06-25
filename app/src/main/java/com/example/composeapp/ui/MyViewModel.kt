71226976dad7f0e08466819c5d440e76ac9a0d1b
ali kamran <ali.kamran.98.98@gmail.com>
2023-06-25 11:03:58 +0330

    map flow with viewmodel compose

--- app/src/main/java/com/example/composeapp/ui/MyViewModel.kt
+++ app/src/main/java/com/example/composeapp/ui/MyViewModel.kt
@@ -1,11 +1,18 @@
 package com.example.composeapp.ui
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 
 class MyViewModel :ViewModel(){
     val myFlow = flow<Int> {
@@ -15,5 +22,30 @@ class MyViewModel :ViewModel(){
         }
     }
 
+    init {
+        backPressureDemo()
+    }
+    private fun backPressureDemo(){
+        val myFlow1 = flow<Int> {
+            for (i in 1..10) {
+                Log.i("MYTAG","Produced $i")
+                emit(i)
+                delay(1000L)
+            }
+        }
 
+        viewModelScope.launch {
+//            myFlow1.buffer().collect{
+            myFlow1.map {
+                it->showMessage(it)
+            }.collect{
+                Log.i("MYTAG","Consumed $it")
+            }
+        }
+
+
+    }
+    fun showMessage(count:Int):String{
+        return "Hello $count"
+    }
 }
