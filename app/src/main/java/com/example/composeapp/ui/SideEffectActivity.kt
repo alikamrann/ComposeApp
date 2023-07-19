2d0c20cc3d4ab7f6c7642ec53aacfef83b245eb8
ali kamran <ali.kamran.98.98@gmail.com>
2023-07-19 20:44:47 +0330

    Side Effec compose

--- app/src/main/java/com/example/composeapp/ui/SideEffectActivity.kt
+++ app/src/main/java/com/example/composeapp/ui/SideEffectActivity.kt
@@ -2,7 +2,6 @@ package com.example.composeapp.ui
 
 import android.content.Context
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -13,14 +12,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,10 +29,12 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composeapp.ui.ui.theme.EffectsDemo1Theme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
 
 class SideEffectActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +43,7 @@ class SideEffectActivity : ComponentActivity() {
             EffectsDemo1Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier.fillMaxSize(),
 
                 ) {
                     MainScreen()
@@ -56,54 +59,64 @@ fun MainScreen(
     modifier: Modifier = Modifier,
     context: Context = LocalContext.current
 ) {
-
+    var round by remember { mutableStateOf(1) }
     var total by remember { mutableStateOf(0.0) }
     var input by remember { mutableStateOf("") }
 
-    Toast.makeText(context, "Please, start counting..", Toast.LENGTH_SHORT).show()
+    val coroutineScope : CoroutineScope = rememberCoroutineScope()
 
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(50.dp),
-        verticalArrangement = Arrangement.SpaceEvenly
-    ) {
-        Text(
-            modifier = modifier.fillMaxWidth(),
-            text = "Total is ${total.toString()}",
-            fontWeight = FontWeight.Bold,
-            fontSize = 30.sp,
-            color = Color.DarkGray
-        )
-        OutlinedTextField(
-            modifier = modifier.fillMaxWidth(),
-            placeholder = { Text("Enter value here") },
-            value = input,
-            onValueChange = {
-                input = it
-            },
-            textStyle = TextStyle(
-                color = Color.LightGray,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold
-            ),
-            label = { Text(text = "New count") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
-        )
 
-        Button(
-            modifier = modifier.fillMaxWidth(),
-            onClick = {
-                total += input.toDouble()
-            }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(50.dp),
+            verticalArrangement = Arrangement.SpaceEvenly
         ) {
             Text(
-                text = "Count",
+                modifier = modifier.fillMaxWidth(),
+                text = "Total is ${total.toString()}",
+                fontWeight = FontWeight.Bold,
                 fontSize = 30.sp,
-                fontWeight = FontWeight.Bold
+                color = Color.DarkGray
             )
+            OutlinedTextField(
+                modifier = modifier.fillMaxWidth(),
+                placeholder = { Text("Enter value here") },
+                value = input,
+                onValueChange = {
+                    input = it
+                },
+                textStyle = TextStyle(
+                    color = Color.LightGray,
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                ),
+                label = { Text(text = "New count") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+
+            Button(
+                modifier = modifier.fillMaxWidth(),
+                onClick = {
+                    total += input.toDouble()
+                    coroutineScope.launch {
+
+                    }
+                    if(total>300){
+                        total = 0.0
+                        input = ""
+                        round++
+                    }
+                }
+            ) {
+                Text(
+                    text = "Count",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
         }
     }
-}
+
 
